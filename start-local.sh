55a2de28e52510574c210d80dbeb1336f5841260
ekemper <ekemper@users.noreply.github.com>
2025-04-24 17:54:04 +0000

    Fix start-local.sh to use npx for esbuild

--- start-local.sh
+++ start-local.sh
@@ -3,7 +3,7 @@
 npm run build
 
 # Build the local server entry point
-esbuild server/local.ts --platform=node --packages=external --bundle --format=esm --outfile=dist/local.js
+npx esbuild server/local.ts --platform=node --packages=external --bundle --format=esm --outfile=dist/local.js
 
 # Run the local server
 NODE_ENV=production node dist/local.js
